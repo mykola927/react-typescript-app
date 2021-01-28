4e77903e249ad18643691f0a8982796575669fc1
UnaiLabrador <Realman1015@outlook.com>
2021-01-28 14:29:39 +0200

    [Update] Initialize Firebase App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA8uWRIIS33KCSL5Ays6hkY69oOZE8X9Zo",
+  authDomain: "react-typescript-chat-ap-d6740.firebaseapp.com",
+  projectId: "react-typescript-chat-ap-d6740",
+  storageBucket: "react-typescript-chat-ap-d6740.appspot.com",
+  messagingSenderId: "759481714764",
+  appId: "1:759481714764:web:7dafa2323bcb1bffd58ce9",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
   return <div className="App">Hello from App</div>;
 }
