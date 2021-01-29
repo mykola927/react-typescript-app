e8bd025b3b4696a0bff428dd2935ebafd416e295
UnaiLabrador <Realman1015@outlook.com>
2021-01-29 16:41:13 +0200

    [Update] Create Group & Menu Tabs

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import CreateGroup from "../../containers/CreateGroup";
+import { Button, Modal, Tabs, Dropdown, Menu } from "antd";
 import { auth } from "../../firebase";
+import { MoreOutlined } from "@ant-design/icons";
+import "./styles.scss";
+import { fetchUsers } from "../../firebase/users";
 
-export default function Menu() {
+export default function MenuContent() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleModalShow = () => {
+    setIsModalVisible(true);
+  };
+
+  const menu = (
+    <Menu>
+      <Menu.Item key="1" onClick={handleModalShow}>
+        New group
+      </Menu.Item>
+      <Menu.Item key="2" onClick={() => auth.signOut()}>
+        Sign out
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <>
-      <header>
-        <button onClick={() => console.log(auth.currentUser)}>get user</button>
-        <button onClick={() => auth.signOut()}>sign out user</button>
-      </header>
+      <div className="app-container__menu__content">
+        <header>
+          <Button
+            onClick={() => console.log(auth.currentUser)}
+            shape="circle"
+            style={{ height: 40, width: 40 }}
+          >
+            M
+          </Button>
+
+          <Button onClick={fetchUsers}>get users</Button>
+
+          <Dropdown.Button
+            overlay={menu}
+            //   shape="circle"
+            icon={<MoreOutlined style={{ fontSize: "1.5rem" }} />}
+          />
+        </header>
+        <div className="tabs-container">
+          <Tabs defaultActiveKey="1" centered>
+            <Tabs.TabPane tab="Coversations" key="1">
+              Hello from converstations
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Contacts" key="2">
+              hello from contacts
+            </Tabs.TabPane>
+          </Tabs>
+        </div>
+      </div>
+      <CreateGroup
+        isModalVisible={isModalVisible}
+        setIsModalVisible={setIsModalVisible}
+      />
     </>
   );
 }
---
+++ src/containers/CreateGroup/index.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { auth } from "../../firebase";
+import { createGroupChat } from "../../firebase/groups";
+import { Modal, Input } from "antd";
+
+interface GroupUsers {
+  uid: string;
+}
+
+interface Props {
+  isModalVisible: boolean;
+  setIsModalVisible: any;
+}
+
+export default function CreateGroup(props: Props) {
+  const [groupName, setGroupName] = useState("");
+  const [userToAdd, setUserToAdd] = useState("");
+  const [groupUsers, setGroupUsers] = useState([] as string[]);
+
+  const { isModalVisible, setIsModalVisible } = props;
+
+  const handleModalHide = () => {
+    setIsModalVisible(false);
+    setGroupName("");
+    setUserToAdd("");
+    setGroupUsers([]);
+  };
+
+  const handleGroupNameInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    setGroupName(event.target.value);
+  };
+
+  const handleUserToAddInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    setUserToAdd(event.target.value);
+  };
+
+  const handleAddUserToGroup = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    // Check if enter was pressed
+    if (event.key === "Enter") {
+      if (userToAdd.length > 1) {
+        setGroupUsers((prevState) => [...prevState, userToAdd]);
+        setUserToAdd("");
+      }
+    }
+  };
+
+  const handleCreateGroup = (users: GroupUsers[]) => {
+    if (auth.currentUser) {
+      createGroupChat({
+        users: users,
+        createdBy: auth.currentUser.uid,
+        groupName: groupName,
+        isPrivate: users.length > 2,
+      });
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        title="Create a Group Chat"
+        visible={isModalVisible}
+        onCancel={handleModalHide}
+        onOk={() => handleCreateGroup([{ uid: "hello" }])}
+        okText="Create Group"
+      >
+        Enter group name:
+        <Input type="text" value={groupName} onChange={handleGroupNameInput} />
+        Enter user email to add to group:
+        <Input
+          type="text"
+          value={userToAdd}
+          onChange={handleUserToAddInput}
+          onKeyPress={handleAddUserToGroup}
+        />
+        Group users
+        <span>{JSON.stringify(groupUsers)}</span>
+      </Modal>
+    </>
+  );
+}
