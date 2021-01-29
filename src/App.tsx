962f733f3fd9799ce33d710d95a100ae3bb5ca10
UnaiLabrador <Realman1015@outlook.com>
2021-01-29 11:30:39 +0200

    [Update] App Initial Layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
--- src/views/Chat/index.tsx
+++ src/views/Chat/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./styles.scss";
 
 export default function Chat() {
-  return <div>Hello from Chat Client!</div>;
+  return (
+    <div className="wrapper">
+      <div className="app-container">
+        <div className="app-container__menu"></div>
+        <div className="app-container__content"></div>
+      </div>
+    </div>
+  );
 }
