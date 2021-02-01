d7aa83bad02ee9cc3682486bbddb81c6520f218f
UnaiLabrador <Realman1015@outlook.com>
2021-02-01 19:32:56 +0200

    Update index.tsx
    Cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { auth } from "../firebase";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React from "react";
+import { Switch, Redirect } from "react-router-dom";
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
 import SignUp from "../views/SignUp";
 import Chat from "../views/Chat";
 
 export default function Routes() {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loadingAuth, setLoadingAuth] = useState(true);
-
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        setAuthenticated(true);
-        setLoadingAuth(false);
-      } else {
-        setAuthenticated(false);
-        setLoadingAuth(false);
-      }
-    });
-  }, []);
-
   return (
     <Switch>
       <PrivateRoute exact path="/" component={Chat} />
