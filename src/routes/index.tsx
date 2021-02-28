d46be10b4e4615ce2e23e4c56422faa66ff6ace3
UnaiLabrador <Realman1015@outlook.com>
2021-02-28 18:36:30 +0200

    [Fix] Private and Public Routes
    memory leaks fix by moving the authState of the user to the routes file as it is mounted and does not cause memory leaks on useeffect.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Redirect } from "react-router-dom";
 import PublicRoute from "./PublicRoute";
 import PrivateRoute from "./PrivateRoute";
 import SignUp from "../views/SignUp";
 import Chat from "../views/Chat";
+import SignIn from "../views/SignIn";
+import { auth } from "../firebase";
 
 export default function Routes() {
+  const [authState, setAuthState] = useState(false);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setAuthState(true);
+      } else {
+        setAuthState(false);
+      }
+    });
+  }, []);
+
   return (
     <Switch>
-      <PrivateRoute exact path="/" component={Chat} />
-      <PublicRoute exact path="/sign-up" component={SignUp} />
+      <PrivateRoute exact path="/" component={Chat} authState={authState} />
+      <PublicRoute
+        exact
+        path="/sign-up"
+        component={SignUp}
+        authState={authState}
+      />
+      <PublicRoute
+        exact
+        path="/sign-in"
+        component={SignIn}
+        authState={authState}
+      />
       <Redirect from="/" to="/sign-up" />
     </Switch>
   );
