f771751321f67bde58f98ff8fe6b5d360fd89079
UnaiLabrador <Realman1015@outlook.com>
2021-02-28 18:38:09 +0200

    [Fix] Private and Public Route Return

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,16 +1,18 @@
 import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = ({ component: Component, authState, ...rest }: any) => {
-  <Route
-    {...rest}
-    render={(props: any) =>
-      authState ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: "/sign-in" }} />
-      )
-    }
-  />;
+  return (
+    <Route
+      {...rest}
+      render={(props: any) =>
+        authState ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/sign-in" }} />
+        )
+      }
+    />
+  );
 };
 
 export default PrivateRoute;
--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,16 +1,18 @@
 import { Route, Redirect } from "react-router-dom";
 
 const PublicRoute = ({ component: Component, authState, ...rest }: any) => {
-  <Route
-    {...rest}
-    render={(props: any) =>
-      !authState ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to={{ pathname: "/" }} />
-      )
-    }
-  />;
+  return (
+    <Route
+      {...rest}
+      render={(props: any) =>
+        !authState ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/" }} />
+        )
+      }
+    />
+  );
 };
 
 export default PublicRoute;
