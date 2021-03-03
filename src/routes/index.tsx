4204c186215186f7cfe8006a93d772b0ebc24742
UnaiLabrador <Realman1015@outlook.com>
2021-03-03 13:23:51 +0200

    [Fix] Loading state

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,44 +6,58 @@ import SignUp from "../views/SignUp";
 import Chat from "../views/Chat";
 import SignIn from "../views/SignIn";
 import { auth } from "../firebase";
+import { Spin } from "antd";
 
 export default function Routes() {
   const [user, setUser] = useState(null as any);
   const [authState, setAuthState] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     auth.onAuthStateChanged((user) => {
       if (user) {
         setAuthState(true);
         setUser(user);
+        setLoading(false);
       } else {
         setAuthState(false);
+        setLoading(false);
       }
     });
   }, []);
 
   return (
-    <Switch>
-      <PrivateRoute
-        exact
-        path="/"
-        component={Chat}
-        authState={authState}
-        user={user}
-      />
-      <PublicRoute
-        exact
-        path="/sign-up"
-        component={SignUp}
-        authState={authState}
-      />
-      <PublicRoute
-        exact
-        path="/sign-in"
-        component={SignIn}
-        authState={authState}
-      />
-      <Redirect from="/" to="/sign-up" />
-    </Switch>
+    <>
+      {loading ? (
+        <Spin
+          size="large"
+          style={{ position: "absolute", left: 0, right: 0, top: "45%" }}
+        />
+      ) : (
+        <Switch>
+          <PrivateRoute
+            exact
+            path="/"
+            component={Chat}
+            authState={authState}
+            user={user}
+          />
+          <PublicRoute
+            exact
+            path="/sign-up"
+            component={SignUp}
+            authState={authState}
+          />
+          <PublicRoute
+            exact
+            path="/sign-in"
+            component={SignIn}
+            authState={authState}
+          />
+          <Redirect from="/" to="/sign-up" />
+        </Switch>
+      )}
+    </>
   );
 }
